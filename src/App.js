9bc1c8ec29ffaa249b4494339829169af070a1b2
Duy Nguyen <nguyenkhanhduy11103@gmail.com>
2024-06-09 10:15:50 +0700

    update content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Thank for client !
+         wellcome back Thank for client  !
         </p>
         
       </header>
